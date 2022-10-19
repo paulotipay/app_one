61a4aba54a1edf285a73551a63cf336402ea62be
paulo <paulo@kodego.ph>
2022-10-20 05:38:13 +0800

    add home screen adapter view

---
+++ app/src/main/java/com/kodego/app/one/ProductAdapter.kt
@@ -0,0 +1,29 @@
+package com.kodego.app.one
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.kodego.app.one.databinding.RowItemBinding
+
+class ProductAdapter(val products:List<Products>):RecyclerView.Adapter<ProductAdapter.ProductViewHolder>() {
+
+    inner class ProductViewHolder(val binding:RowItemBinding): RecyclerView.ViewHolder(binding.root)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ProductViewHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        val binding = RowItemBinding.inflate(layoutInflater, parent, false)
+        return ProductViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ProductViewHolder, position: Int) {
+        holder.binding.apply {
+            imgProduct.setImageResource(products[position].imageName)
+            tvItemName.text = products[position].itemName
+            tvDescription.text = products[position].itemDescription
+        }
+    }
+
+    override fun getItemCount(): Int {
+       return products.size
+    }
+}
---
+++ app/src/main/java/com/kodego/app/one/Products.kt
@@ -0,0 +1,3 @@
+package com.kodego.app.one
+
+data class Products(val imageName:Int,val itemName:String, val itemDescription:String)
